ae794a123cd1a26e8f49978db2e185d245ffcf8c
Ron Gruesbeck <rongruesbeck@gmail.com>
2017-12-05 00:57:51 +0000

    work on CORS issues

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 var router = require('./routes');
 
 var server = http.createServer(function (req, res) {
-  console.log(req.method, req.url);
+  console.log(req.method, req.url, req.headers);
   var path = "".concat(req.method, ' ', req.url);
   var match = router.match(path);
   if (match) match.fn(req, res, match);
